07aa59321eb3c6fb84b5dae231c30506118928e1
chrishoran8 <horan_chris@hotmail.co.uk>
2023-06-21 16:56:13 +0100

    Add project files.

---
+++ FlagBitwiseLab-ForStudents/FlagBitwiseLab_ForStudents.py
@@ -0,0 +1,29 @@
+#create a flag with a set flag
+
+#create an appropriate mask
+
+#1 use & to check if flag set/reset
+
+#2 reset(assign zero) to bit
+
+#check 2
+
+#3 use | to assign bit to 1 
+
+#check 3
+
+#4 use ^ to make it the opposite/negate your bit
+
+#check 4
+
+#5 use ^ to make it the opposite/negate your bit
+
+#check 5
+
+#The above should output (ingoring the # of course)
+#bit is set
+#bit is reset
+#bit is set
+#bit is reset
+#bit is set
+#Press any key to continue . . .
